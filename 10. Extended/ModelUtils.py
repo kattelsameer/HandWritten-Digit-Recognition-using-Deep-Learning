Correct softmax function to avoid NaN during training

10. Extended/ModelUtils.py
@@ -116,7 +116,7 @@ def softmax(Z):
                  [0.13925557]
                  [0.12280831]]
     """
-    shift = Z - np.max(Z)  # Avoiding underflow or overflow errors due to floating point instability in softmax
+    shift = Z - np.max(Z, axis = 0)# Avoiding underflow or overflow errors due to floating point instability in softmax
     t = np.exp(shift)
     A = np.divide(t, np.sum(t, axis=0))
 
@@ -581,7 +581,7 @@ def visualize_training_results(train_accs, val_accs, train_loss, val_loss):
     axes[0].plot(np.squeeze(train_loss), label='Training Loss', color='blue')
     axes[0].plot(np.squeeze(val_loss), label='Validation Loss', color='red')
     axes[0].legend(loc='upper right')  # setting up legend location to upper right corner of the plot
-    axes[0].set_title("Training and Validation Loss ", fontsize=16, pad=10)
+    axes[0].set_title("Training and Validation Loss Graph", fontsize=16, pad=10)
     axes[0].set_xlabel("No. of Epochs", fontsize=12)
     axes[0].set_ylabel("Loss", fontsize=12)
     axes[0].set_ylim(bottom=0)
@@ -591,7 +591,7 @@ def visualize_training_results(train_accs, val_accs, train_loss, val_loss):
     axes[1].plot(np.squeeze(train_accs), label='Training Accuracy', color='blue')
     axes[1].plot(np.squeeze(val_accs), label='Validation Accuracy', color='red')
     axes[1].legend(loc='lower right')  # setting up legend location to lower right corner of the plot
-    axes[1].set_title("Accuracy ", fontsize=16, pad=10)
+    axes[1].set_title("Training and Validation Accuracy Graph ", fontsize=16, pad=10)
     axes[1].set_xlabel("No. of Epochs", fontsize=12)
     axes[1].set_ylabel("Accuracy", fontsize=12)
     axes[1].set_ylim(top=1)